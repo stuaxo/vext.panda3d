a1cefb8b1fbdefa4e5ddd175b3f7529adbb302d0
Stuart Axon <stuaxo2@yahoo.com>
2020-08-06 15:23:04 +0100

    Update supported python versions.

--- setup.py
+++ setup.py
@@ -81,6 +81,10 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     # What does your project relate to?
     keywords="virtualenv panda3d 3d vext",
